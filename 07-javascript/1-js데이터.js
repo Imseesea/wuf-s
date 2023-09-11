c3948001eea16b438de922ca1390fd9a31e388ee
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-09-11 11:45:31 +0900

    javascript 00,01

---
+++ 07-javascript/1-js데이터.js
@@ -0,0 +1,12 @@
+//string (문자열)
+
+const string1 = "hello"; //큰다옴표 가능
+const string2 = "imseesea"; //작은따옴표 가능
+const string3 = `뽀로로${string1} ${1 + 1} `; //백틱 가능
+
+console.log(string1, string2, string3);
+
+// nuumber (숫자)
+const nuumber = 123;
+
+console.log(number);
